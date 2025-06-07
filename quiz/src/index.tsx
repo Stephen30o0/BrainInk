042b110537a75726bd418cebb53c72cdf7b1a8ff
dn004 <d.ntwari@alustudent.com>
2025-06-07 20:49:26 +0200

    redeploy

--- quiz/src/index.tsx
+++ quiz/src/index.tsx
@@ -1,5 +1,18 @@
 import './index.css';
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom/client';
 import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import { AudioProvider } from '../../src/components/shared/AudioManager'; // Adjusted path
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <AudioProvider>
+        <App />
+      </AudioProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Failed to find the root element');
+}
