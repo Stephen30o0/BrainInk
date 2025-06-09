d38a878f6f217169d294f9f50d89e780b58475c2
dn004 <d.ntwari@alustudent.com>
2025-06-09 21:07:53 +0200

    upload

--- quiz/vite.config.ts
+++ quiz/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://kana-backend-app.onrender.com',
         changeOrigin: true,
         // secure: false, // Uncomment if your backend is http
       }
